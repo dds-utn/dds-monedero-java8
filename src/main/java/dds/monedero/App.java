658741464a97d0717edfe5e75d5b110acf928fbd
gastonprieto <gaston.prieto.utn@gmail.com>
2014-04-21 22:12:28 -0300

    Proyecto inicial

---
+++ src/main/java/dds/monedero/App.java
@@ -0,0 +1,13 @@
+package dds.monedero;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
