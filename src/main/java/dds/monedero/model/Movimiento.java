9727894bf95ddd32b446a27e86e1882f1fde111e
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2014-04-25 22:09:07 -0300

    Sacamos el type test de calcularValor

--- src/main/java/dds/monedero/model/Movimiento.java
+++ src/main/java/dds/monedero/model/Movimiento.java
@@ -48,11 +48,5 @@ public abstract class Movimiento {
     cuenta.agregarMovimiento(fecha, monto, esDeposito);
   }
 
-  public double calcularValor(Cuenta cuenta) {
-    if (esDeposito) {
-      return cuenta.getSaldo() + getMonto();
-    } else {
-      return cuenta.getSaldo() - getMonto();
-    }
-  }
+  public abstract double calcularValor(Cuenta cuenta);
 }
