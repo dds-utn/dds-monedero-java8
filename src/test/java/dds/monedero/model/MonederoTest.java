a02997175d4fc94b5e51bf14c51f90cb271c6fd5
nanderson87 <nicolas.anderson@mercadolibre.com>
2021-04-26 18:58:32 -0300

    Elimina verificación del mensaje en assert

--- src/test/java/dds/monedero/model/MonederoTest.java
+++ src/test/java/dds/monedero/model/MonederoTest.java
@@ -7,8 +7,6 @@ import dds.monedero.exceptions.SaldoMenorException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDate;
-
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class MonederoTest {
@@ -26,11 +24,7 @@ public class MonederoTest {
 
   @Test
   void PonerMontoNegativo() {
-    final int montoNegativo = -1500;
-
-    assertThrows(MontoNegativoException.class, () -> cuenta.poner(montoNegativo),
-        montoNegativo + ": el monto a ingresar debe ser un valor positivo"
-    );
+    assertThrows(MontoNegativoException.class, () -> cuenta.poner(-1500));
   }
 
   @Test
@@ -47,40 +41,28 @@ public class MonederoTest {
           cuenta.poner(456);
           cuenta.poner(1900);
           cuenta.poner(245);
-        },
-        "Ya excedio los 3 depositos diarios"
-    );
+    });
   }
 
   @Test
   void ExtraerMasQueElSaldo() {
-    final double saldoActual = 90;
-
     assertThrows(SaldoMenorException.class, () -> {
-          cuenta.setSaldo(saldoActual);
+          cuenta.setSaldo(90);
           cuenta.sacar(1001);
-        },
-        "No puede sacar mas de " + saldoActual + " $"
-    );
+    });
   }
 
   @Test
   public void ExtraerMasDe1000() {
-    final double limite = cuenta.getMontoExtraidoA(LocalDate.now());
-    cuenta.setSaldo(5000);
-
-    assertThrows(MaximoExtraccionDiarioException.class, () -> cuenta.sacar(1001),
-        "No puede extraer mas de $ " + 1000 + " diarios, límite: " + limite
-    );
+    assertThrows(MaximoExtraccionDiarioException.class, () -> {
+      cuenta.setSaldo(5000);
+      cuenta.sacar(1001);
+    });
   }
 
   @Test
   public void ExtraerMontoNegativo() {
-    final double montoNegativo = -500;
-
-    assertThrows(MontoNegativoException.class, () -> cuenta.sacar(montoNegativo),
-        montoNegativo + ": el monto a ingresar debe ser un valor positivo"
-    );
+    assertThrows(MontoNegativoException.class, () -> cuenta.sacar(-500));
   }
 
 }
