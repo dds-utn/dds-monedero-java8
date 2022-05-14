475e9bfafaaf6baed9cac591e17c06be521c6ea5
Franco Leonardo Bulgarelli <franco@mumuki.org>
2022-05-13 22:31:04 -0300

    Eliminar parámetro superfluo

--- src/main/java/dds/monedero/model/Deposito.java
+++ src/main/java/dds/monedero/model/Deposito.java
@@ -3,7 +3,7 @@ package dds.monedero.model;
 import java.time.LocalDate;
 
 public class Deposito extends Movimiento {
-  public Deposito(LocalDate fecha, double monto, boolean esDeposito) {
-    super(fecha, monto, esDeposito);
+  public Deposito(LocalDate fecha, double monto) {
+    super(fecha, monto, true);
   }
 }
--- src/main/java/dds/monedero/model/Extraccion.java
+++ src/main/java/dds/monedero/model/Extraccion.java
@@ -3,7 +3,7 @@ package dds.monedero.model;
 import java.time.LocalDate;
 
 public class Extraccion extends Movimiento {
-  public Extraccion(LocalDate fecha, double monto, boolean esDeposito) {
-    super(fecha, monto, esDeposito);
+  public Extraccion(LocalDate fecha, double monto) {
+    super(fecha, monto, false);
   }
 }
