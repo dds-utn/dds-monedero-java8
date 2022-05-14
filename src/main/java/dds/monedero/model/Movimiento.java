7303eb2d799828fbb873999a7d74f84ad4c1e59e
Franco Leonardo Bulgarelli <franco@mumuki.org>
2022-05-13 22:19:50 -0300

    Removiendo primitive obsession

--- src/main/java/dds/monedero/model/Movimiento.java
+++ src/main/java/dds/monedero/model/Movimiento.java
@@ -46,7 +46,7 @@ public class Movimiento {
 
   public void agregateA(Cuenta cuenta) {
     cuenta.setSaldo(calcularValor(cuenta));
-    cuenta.agregarMovimiento(fecha, monto, esDeposito);
+    cuenta.agregarMovimiento(this);
   }
 
   public double calcularValor(Cuenta cuenta) {
