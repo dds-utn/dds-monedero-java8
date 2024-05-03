820ffd7c77547689e1841e1c17103b9503acdd54
Franco Leonardo Bulgarelli <franco@mumuki.org>
2024-05-03 20:41:04 -0300

    Usando DisplayName

--- src/test/java/dds/monedero/model/MonederoTest.java
+++ src/test/java/dds/monedero/model/MonederoTest.java
@@ -5,6 +5,7 @@ import dds.monedero.exceptions.MaximoExtraccionDiarioException;
 import dds.monedero.exceptions.MontoNegativoException;
 import dds.monedero.exceptions.SaldoMenorException;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -20,16 +21,19 @@ public class MonederoTest {
   }
 
   @Test
+  @DisplayName("Es posible poner $1500 en una cuenta vacía")
   void Poner() {
     cuenta.poner(BigDecimal.valueOf(1500));
   }
 
   @Test
+  @DisplayName("No es posible poner montos negativos")
   void PonerMontoNegativo() {
     assertThrows(MontoNegativoException.class, () -> cuenta.poner(BigDecimal.valueOf(-1500)));
   }
 
   @Test
+  @DisplayName("Es posible realizar múltiples depósitos consecutivos")
   void TresDepositos() {
     cuenta.poner(BigDecimal.valueOf(1500));
     cuenta.poner(BigDecimal.valueOf(456));
@@ -37,6 +41,7 @@ public class MonederoTest {
   }
 
   @Test
+  @DisplayName("No es posible superar la máxima cantidad de depositos diarios")
   void MasDeTresDepositos() {
     assertThrows(MaximaCantidadDepositosException.class, () -> {
       cuenta.poner(BigDecimal.valueOf(1500));
@@ -47,6 +52,7 @@ public class MonederoTest {
   }
 
   @Test
+  @DisplayName("No es posible extraer más que el saldo disponible")
   void ExtraerMasQueElSaldo() {
     assertThrows(SaldoMenorException.class, () -> {
       cuenta.setSaldo(BigDecimal.valueOf(90));
@@ -55,7 +61,8 @@ public class MonederoTest {
   }
 
   @Test
-  public void ExtraerMasDe1000() {
+  @DisplayName("No es posible extraer más que el límite diario")
+  void ExtraerMasDe1000() {
     assertThrows(MaximoExtraccionDiarioException.class, () -> {
       cuenta.setSaldo(BigDecimal.valueOf(5000));
       cuenta.sacar(BigDecimal.valueOf(1001));
@@ -63,6 +70,7 @@ public class MonederoTest {
   }
 
   @Test
+  @DisplayName("No es posible extraer un monto negativo")
   public void ExtraerMontoNegativo() {
     assertThrows(MontoNegativoException.class, () -> cuenta.sacar(BigDecimal.valueOf(-500)));
   }
